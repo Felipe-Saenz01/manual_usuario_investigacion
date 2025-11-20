9d529672522c90691cfc008db4635312d4b8533f
Juan Felipe Saenz Niño <saenzfelipe2001@gmail.com>
2025-11-20 15:58:53 -0500

    Add unitropico logo image to assets, add configuration and deploy file configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,31 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki';
+import { fileURLToPath } from 'url';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://felipe-saenz01.github.io/manual_usuario_investigacion',
+	base: '/manual_usuario_investigacion',
+	vite: {
+		resolve: {
+			alias: {
+				'@assets': fileURLToPath(new URL('./src/assets/', import.meta.url)),
+			},
+		},
+	},
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			plugins: [starlightThemeFlexoki()],
+			logo: {
+				src: './src/assets/unitropico-logo.png'
+			},
+			title: 'Manual Procesos Investigativos Unitrópico',
+			defaultLocale: 'root',
+			locales: {
+				root: { label: 'Español', lang: 'es' },
+			},
 			sidebar: [
 				{
 					label: 'Guides',
